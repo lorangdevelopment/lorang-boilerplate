f4199d5360f26bb99a4199b4e5d51c386a062038
Lorang <lorangdevelopment@gmail.com>
2024-10-15 13:32:33 +0200

    Finished the +error.svelte

--- lorang-boilerplate/src/routes/+error.svelte
+++ lorang-boilerplate/src/routes/+error.svelte
@@ -1,11 +1,71 @@
 <script lang="ts">
-	import { page } from '$app/stores';
+	import { page } from "$app/stores";
 </script>
 
 <svelte:head>
-	<title>{$page.status}: {$page.error?.message ?? 'An error occurred'}</title>
+	<title>{$page.status}</title>
 </svelte:head>
 
-<div class="sr-only">
-	<h1>{$page.status}: {$page.error?.message ?? 'An error occurred'}</h1>
-</div>
+<section class="error overflow-hidden position-relative">
+	<div class="error__row" style="--text-color: #ffffff; --bg-color: #000000;">
+		<div class="error__scroll-content">
+			<div class="sr-only">
+				<h1>{$page.status}: {$page.error?.message ?? 'An error occurred'}</h1>
+			</div>
+			<div class="error__content">
+				<div class="error__message tt-uppercase --large --fowards">
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+				</div>
+				<div class="error__message tt-uppercase --small --backwards">
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+				</div>
+				<div class="error__message tt-uppercase --large --backwards">
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+				</div>
+				<div class="error__message tt-uppercase --small  --fowards">
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+				</div>
+				<div class="error__message tt-uppercase --large --fowards">
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+				</div>
+				<div class="error__message tt-uppercase --small  --backwards">
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+					<div>{$page.status}: {$page.error?.message ?? 'An error occurred'}</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
